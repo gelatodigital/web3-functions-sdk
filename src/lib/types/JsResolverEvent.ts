4be021940d43cc28489ae6feb70f12a3069d35de
Sylvain Goumy <goumsss@protonmail.com>
2022-12-02 14:26:45 +0100

    refactor: remove unused LogLevel type

--- src/lib/types/JsResolverEvent.ts
+++ src/lib/types/JsResolverEvent.ts
@@ -1,8 +1,6 @@
 import { JsResolverContextData } from "./JsResolverContext";
 import { JsResolverResult } from "./JsResolverResult";
 
-export type LogLevel = "info" | "debug" | "warn" | "error";
-
 export type JsResolverEvent =
   | { action: "start"; data: { context: JsResolverContextData } }
   | { action: "error"; data: { error: Error } }
