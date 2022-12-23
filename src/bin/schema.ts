b7fae80df64b30d984961ffc57acccd0e5f377e1
Sylvain Goumy <goumsss@protonmail.com>
2022-12-23 13:32:39 +0100

    feat: refactor scripts in binary

--- src/bin/schema.ts
+++ src/bin/schema.ts
@@ -1,17 +1,14 @@
+#! /usr/bin/env node
 import "dotenv/config";
 import colors from "colors/safe";
-import { JsResolverUploader } from "@gelatonetwork/js-resolver-sdk/uploader";
+import { JsResolverUploader } from "../lib/uploader";
 
 const OK = colors.green("✓");
-const KO = colors.red("✗");
-
-const schema = async () => {
-  const cid = process.argv[2];
+export default async function schema() {
+  const cid = process.argv[3];
 
   if (!cid) throw new Error("JsResolver CID missing");
 
   const schema = await JsResolverUploader.fetchSchema(cid);
   console.log(` ${OK} Fetched JsResolver schema: ${JSON.stringify(schema)}`);
-};
-
-schema().catch((err) => console.error(` ${KO} ${err.message}`));
+}
