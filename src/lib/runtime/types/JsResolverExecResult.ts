24dca4fd0e254d1677942f39ee2dec71111ce6bf
Brandon Chuah <songlin.chuah@gmail.com>
2023-01-11 17:05:40 +0900

    chore: return storage in JsResolverExecResult

--- src/lib/runtime/types/JsResolverExecResult.ts
+++ src/lib/runtime/types/JsResolverExecResult.ts
@@ -1,3 +1,4 @@
+import { JsResolverStorage } from "../../types";
 import { JsResolverResult } from "../../types/JsResolverResult";
 
 type JsResolverExecStats = {
@@ -10,11 +11,13 @@ type JsResolverExecStats = {
 type JsResolverExecSuccess = JsResolverExecStats & {
   success: true;
   result: JsResolverResult;
+  storage: JsResolverStorage;
 };
 
 type JsResolverExecFail = JsResolverExecStats & {
   success: false;
   error: Error;
+  storage: JsResolverStorage;
 };
 
 export type JsResolverExec = JsResolverExecSuccess | JsResolverExecFail;
