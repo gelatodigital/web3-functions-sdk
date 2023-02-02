bbae8b0cca112c0dcee9a23f9dd030c211ef1cff
Sylvain Goumy <goumsss@protonmail.com>
2023-02-02 17:47:28 +0100

    feat: add task creation link on deploy

--- src/lib/binaries/deploy.ts
+++ src/lib/binaries/deploy.ts
@@ -7,5 +7,10 @@ const web3FunctionSrcPath = process.argv[3] ?? "./src/web3Functions/index.ts";
 
 export default async function deploy() {
   const cid = await Web3FunctionBuilder.deploy(web3FunctionSrcPath);
-  console.log(` ${OK} Web3Function deployed to ipfs. CID: ${cid}`);
+  console.log(` ${OK} Web3Function deployed to ipfs.`);
+  console.log(` ${OK} CID: ${cid}`);
+  console.log(
+    `\nTo create a task that runs your Web3 Function every minute, visit:`
+  );
+  console.log(`> https://beta.app.gelato.network/new-task?cid=${cid}`);
 }
