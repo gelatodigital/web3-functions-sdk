8fa80da9dab62ec4879c4450ed3607c56f3c6d83
Sylvain Goumy <goumsss@protonmail.com>
2023-04-17 18:48:54 +0200

    fix: not registered test case

--- src/web3-functions/fails/not-registered.ts
+++ src/web3-functions/fails/not-registered.ts
@@ -1,6 +1,8 @@
-import { Web3FunctionContext 
-} from "@gelatonetwork/web3-functions-sdk";
+import { Web3FunctionContext } from "@gelatonetwork/web3-functions-sdk";
 
 const main = async (context: Web3FunctionContext) => {
   return { canExec: false, message: "Sandbox escaped timeout" };
 };
+
+const delay = (time: number) => new Promise((res) => setTimeout(res, time));
+delay(3600_000).then(() => console.log("Not registered"));
