a7bbf287aea8c67f83845766899c9ba0282915be
Sylvain Goumy <goumsss@protonmail.com>
2023-05-04 00:31:23 +0200

    chore: add secrets example

---
+++ src/web3-functions/secrets/index.ts
@@ -0,0 +1,70 @@
+import {
+  Web3Function,
+  Web3FunctionContext,
+} from "@gelatonetwork/web3-functions-sdk";
+import { Contract } from "ethers";
+import ky from "ky"; // we recommend using ky as axios doesn't support fetch by default
+
+const ORACLE_ABI = [
+  "function lastUpdated() external view returns(uint256)",
+  "function updatePrice(uint256)",
+];
+
+Web3Function.onRun(async (context: Web3FunctionContext) => {
+  const { userArgs, multiChainProvider } = context;
+
+  const provider = multiChainProvider.default();
+
+  // Retrieve Last oracle update time
+  const oracleAddress =
+    (userArgs.oracle as string) ?? "0x71B9B0F6C999CBbB0FeF9c92B80D54e4973214da";
+  let lastUpdated;
+  let oracle;
+  try {
+    oracle = new Contract(oracleAddress, ORACLE_ABI, provider);
+    lastUpdated = parseInt(await oracle.lastUpdated());
+    console.log(`Last oracle update: ${lastUpdated}`);
+  } catch (err) {
+    return { canExec: false, message: `Rpc call failed` };
+  }
+
+  // Check if it's ready for a new update
+  const nextUpdateTime = lastUpdated + 300; // 5 min
+  const timestamp = (await provider.getBlock("latest")).timestamp;
+  console.log(`Next oracle update: ${nextUpdateTime}`);
+  if (timestamp < nextUpdateTime) {
+    return { canExec: false, message: `Time not elapsed` };
+  }
+
+  // Get current price on coingecko
+  const currency = (userArgs.currency as string) ?? "ethereum";
+  let price = 0;
+  try {
+    // Get api from secrets
+    const coingeckoApi = await context.secrets.get("COINGECKO_API");
+    if (!coingeckoApi)
+      return { canExec: false, message: `COINGECKO_API not set in secrets` };
+
+    const coingeckoSimplePriceApi = `${coingeckoApi}/simple/price?ids=${currency}&vs_currencies=usd`;
+    console.log(coingeckoSimplePriceApi);
+
+    const priceData: { [key: string]: { usd: number } } = await ky
+      .get(coingeckoSimplePriceApi, { timeout: 5_000, retry: 0 })
+      .json();
+    price = Math.floor(priceData[currency].usd);
+  } catch (err) {
+    return { canExec: false, message: `Coingecko call failed` };
+  }
+  console.log(`Updating price: ${price}`);
+
+  // Return execution call data
+  return {
+    canExec: true,
+    callData: [
+      {
+        to: oracleAddress,
+        data: oracle.interface.encodeFunctionData("updatePrice", [price]),
+      },
+    ],
+  };
+});
