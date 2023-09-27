e7a67ed57ebb358b19c99554f15dcfbeaae7a916
Muhammed Kadir Yücel <kadir@gelato.digital>
2023-09-27 13:18:36 +0300

    refactor: reduce cyclomatic complexity of test binary

--- src/lib/binaries/test.ts
+++ src/lib/binaries/test.ts
@@ -5,7 +5,12 @@ import { Web3FunctionBuilder } from "../builder";
 import { Web3FunctionLoader } from "../loader";
 import { MultiChainProviderConfig } from "../provider";
 import { Web3FunctionRunner } from "../runtime";
-import { Web3FunctionContextData, Web3FunctionUserArgs } from "../types";
+import {
+  Web3FunctionContextData,
+  Web3FunctionExec,
+  Web3FunctionStorageWithSize,
+  Web3FunctionUserArgs,
+} from "../types";
 
 const STD_TIMEOUT = 10;
 const STD_RPC_LIMIT = 10;
@@ -35,26 +40,142 @@ export interface CallConfig {
 
 export type RunTime = "docker" | "thread";
 
-export default async function test(callConfig?: Partial<CallConfig>) {
-  let userArgs: Web3FunctionUserArgs = callConfig?.userArgs ?? {};
-  let chainId = callConfig?.chainId ?? 5;
-  const multiChainProviderConfig: MultiChainProviderConfig =
-    callConfig?.multiChainProviderConfig ?? {
-      5: new StaticJsonRpcProvider("https://eth-goerli.public.blastapi.io"),
-    };
-  let runtime: RunTime = callConfig?.runtime ?? "thread";
-  let debug = callConfig?.debug ?? false;
-  let showLogs = callConfig?.showLogs ?? false;
-  let storage = callConfig?.storage ?? {};
-  let secrets = callConfig?.secrets ?? {};
-  let log = callConfig?.log;
+function logWithStatus(status: string, message: string, indent = 0) {
+  const indentation = " ".repeat(indent);
+  console.log(`${indentation}${status} ${message}`);
+}
 
-  const web3FunctionPath =
-    callConfig?.w3fPath ??
-    process.argv[3] ??
-    path.join(process.cwd(), "src", "web3-functions", "index.ts");
+function logStorage(storage: Web3FunctionStorageWithSize) {
+  // Show storage update
+  if (storage?.state === "updated") {
+    console.log(`\nSimulated Web3Function Storage update:`);
+    Object.entries(storage.storage).forEach(([key, value]) =>
+      logWithStatus(OK, `${key}: ${colors.green(`'${value}'`)}`, 1)
+    );
+  }
+}
+
+function logDurationStats(res: Web3FunctionExec) {
+  if (res.throttled.duration) {
+    logWithStatus(KO, `Duration: ${res.duration.toFixed(2)}s`, 1);
+  } else if (res.duration > STD_TIMEOUT) {
+    logWithStatus(
+      WARN,
+      `Duration: ${res.duration.toFixed(
+        2
+      )}s (Runtime is above Standard plan limit: ${STD_TIMEOUT}s!)`,
+      1
+    );
+  } else {
+    logWithStatus(OK, `Duration: ${res.duration.toFixed(2)}s`, 1);
+  }
+}
+
+function logMemoryStats(res: Web3FunctionExec) {
+  logWithStatus(
+    res.throttled.memory ? KO : OK,
+    `Memory: ${res.memory.toFixed(2)}mb`,
+    1
+  );
+}
+
+function logStorageStats(res: Web3FunctionExec) {
+  if (res.success && res.throttled.storage) {
+    logWithStatus(
+      KO,
+      `Storage: ${res.storage.size.toFixed(
+        2
+      )}kb - Storage usage exceeds limit!`,
+      1
+    );
+  } else if (res.success && res.storage?.size > STD_STORAGE_LIMIT) {
+    logWithStatus(
+      KO,
+      `Storage: ${res.storage.size.toFixed(
+        2
+      )}kb (Storage usage is above Standard plan limit: ${STD_STORAGE_LIMIT}kb!)`,
+      1
+    );
+  } else if (res.success && res.storage?.size > 0) {
+    logWithStatus(OK, `Storage: ${res.storage.size.toFixed(2)}kb`, 1);
+  }
+}
+
+function logNetworkStats(res: Web3FunctionExec) {
+  let networkMessage = `Network: ${res.network.nbRequests} req [${
+    res.throttled.download ? KO : ""
+  } DL: ${res.network.download.toFixed(2)}kb / UL: ${
+    res.throttled.upload ? KO : ""
+  } ${res.network.upload.toFixed(2)}kb]`;
+
+  if (res.throttled.networkRequest) {
+    networkMessage += ` ${`(${res.network.nbThrottled} req throttled - Please reduce your network usage!)`}`;
+  }
+  logWithStatus(res.throttled.networkRequest ? KO : OK, networkMessage, 1);
+}
+
+function logRPCStats(res: Web3FunctionExec) {
+  if (res.throttled.rpcRequest) {
+    logWithStatus(
+      KO,
+      `Rpc calls: ${
+        res.rpcCalls.total
+      } ${`(${res.rpcCalls.throttled} throttled - Please reduce your rpc usage!)`}`,
+      1
+    );
+  } else if (res.rpcCalls.total > STD_RPC_LIMIT) {
+    logWithStatus(
+      WARN,
+      `Rpc calls: ${
+        res.rpcCalls.total
+      } ${`(RPC usage is above Standard plan limit: ${STD_RPC_LIMIT}!)`}`,
+      1
+    );
+  } else {
+    logWithStatus(OK, `Rpc calls: ${res.rpcCalls.total}`, 1);
+  }
+}
+
+function logResult(res: Web3FunctionExec) {
+  // Show Web3Function result
+  console.log(`\nWeb3Function Result:`);
+  if (res.success) {
+    logWithStatus(OK, `Return value: ${JSON.stringify(res.result)}`, 1);
+    logStorage(res.storage);
+  } else {
+    logWithStatus(KO, `Error: ${res.error.message}`, 1);
+  }
+
+  // Show runtime stats
+  console.log(`\nWeb3Function Runtime stats:`);
+  logDurationStats(res);
+  logMemoryStats(res);
+  logStorageStats(res);
+  logNetworkStats(res);
+  logRPCStats(res);
+}
+
+export default async function test(callConfig?: Partial<CallConfig>) {
+  const defaultCallConfig: CallConfig = {
+    userArgs: {},
+    chainId: 5,
+    multiChainProviderConfig: {
+      5: new StaticJsonRpcProvider("https://eth-goerli.public.blastapi.io"),
+    },
+    runtime: "thread",
+    debug: false,
+    showLogs: false,
+    storage: {},
+    secrets: {},
+    w3fPath:
+      process.argv[3] ??
+      path.join(process.cwd(), "src", "web3-functions", "index.ts"),
+  };
 
   if (!callConfig) {
+    callConfig = {};
+    callConfig.multiChainProviderConfig = {};
+
     if (!process.env.PROVIDER_URLS) {
       console.error(`Missing PROVIDER_URLS in .env file`);
       process.exit();
@@ -64,49 +185,70 @@ export default async function test(callConfig?: Partial<CallConfig>) {
     for (const url of providerUrls) {
       const provider = new StaticJsonRpcProvider(url);
       const chainId = (await provider.getNetwork()).chainId;
-      multiChainProviderConfig[chainId] = provider;
+      callConfig.multiChainProviderConfig[chainId] = provider;
     }
 
-    if (process.argv.length > 2) {
-      process.argv.slice(3).forEach((arg) => {
-        if (arg.startsWith("--debug")) {
-          debug = true;
-        } else if (arg.startsWith("--logs")) {
-          showLogs = true;
-        } else if (arg.startsWith("--runtime=")) {
-          const type = arg.split("=")[1];
-          runtime = type === "docker" ? "docker" : "thread";
-        } else if (arg.startsWith("--chain-id")) {
-          chainId = parseInt(arg.split("=")[1]) ?? chainId;
-        }
-      });
+    for (const arg of process.argv.slice(3)) {
+      if (arg.startsWith("--debug")) {
+        callConfig.debug = true;
+      } else if (arg.startsWith("--logs")) {
+        callConfig.showLogs = true;
+      } else if (arg.startsWith("--runtime=")) {
+        const type = arg.split("=")[1];
+        callConfig.runtime = type === "docker" ? "docker" : "thread";
+      } else if (arg.startsWith("--chain-id")) {
+        callConfig.chainId = parseInt(arg.split("=")[1]);
+      }
     }
 
     // Load Web3Function details (userArgs, secrets, storage)
-    const parsedPathParts = path.parse(web3FunctionPath).dir.split(path.sep);
+    const parsedPathParts = path
+      .parse(defaultCallConfig.w3fPath)
+      .dir.split(path.sep);
     const w3fName = parsedPathParts.pop() ?? "";
     const w3fRootDir = parsedPathParts.join(path.sep);
     const w3fDetails = Web3FunctionLoader.load(w3fName, w3fRootDir);
-    userArgs = w3fDetails.userArgs;
-    secrets = w3fDetails.secrets;
-    storage = w3fDetails.storage;
-    log = w3fDetails.log;
+    callConfig.userArgs = w3fDetails.userArgs;
+    callConfig.secrets = w3fDetails.secrets;
+    callConfig.storage = w3fDetails.storage;
+    callConfig.log = w3fDetails.log;
+  }
+
+  // Overwrite default with the callConfig
+  for (const key of Object.keys(callConfig)) {
+    if (callConfig[key]) {
+      defaultCallConfig[key] = callConfig[key];
+    }
   }
 
   // Build Web3Function
   console.log(`Web3Function building...`);
 
-  const buildRes = await Web3FunctionBuilder.build(web3FunctionPath, { debug });
+  const {
+    w3fPath,
+    debug,
+    secrets,
+    storage,
+    chainId,
+    userArgs,
+    runtime,
+    showLogs,
+    multiChainProviderConfig,
+    log,
+  } = defaultCallConfig;
+
+  const buildRes = await Web3FunctionBuilder.build(w3fPath, { debug });
   console.log(`\nWeb3Function Build result:`);
-  if (buildRes.success) {
-    console.log(` ${OK} Schema: ${buildRes.schemaPath}`);
-    console.log(` ${OK} Built file: ${buildRes.filePath}`);
-    console.log(` ${OK} File size: ${buildRes.fileSize.toFixed(2)}mb`);
-    console.log(` ${OK} Build time: ${buildRes.buildTime.toFixed(2)}ms`);
-  } else {
-    console.log(` ${KO} Error: ${buildRes.error.message}`);
+
+  if (!buildRes.success) {
+    logWithStatus(KO, `Error: ${buildRes.error.message}`, 1);
     return;
   }
+
+  logWithStatus(OK, `Schema: ${buildRes.schemaPath}`, 1);
+  logWithStatus(OK, `Built file: ${buildRes.filePath}`, 1);
+  logWithStatus(OK, `File size: ${buildRes.fileSize.toFixed(2)}mb`, 1);
+  logWithStatus(OK, `Build time: ${buildRes.buildTime.toFixed(2)}ms`, 1);
 
   // Prepare mock content for test
   const context: Web3FunctionContextData = {
@@ -141,18 +283,16 @@ export default async function test(callConfig?: Partial<CallConfig>) {
   const script = buildRes.filePath;
 
   // Validate user args against schema
-  if (Object.keys(buildRes.schema.userArgs).length > 0) {
-    console.log(`\nWeb3Function user args validation:`);
-    try {
-      runner.validateUserArgs(buildRes.schema.userArgs, userArgs);
+  console.log(`\nWeb3Function user args validation:`);
+  try {
+    runner.validateUserArgs(buildRes.schema.userArgs, userArgs);
 
-      Object.keys(context.userArgs).forEach((key) => {
-        console.log(` ${OK} ${key}:`, context.userArgs[key]);
-      });
-    } catch (err) {
-      console.log(` ${KO} ${err.message}`);
-      return;
-    }
+    Object.keys(context.userArgs).forEach((key) => {
+      logWithStatus(OK, `${key}: ${context.userArgs[key]}`, 1);
+    });
+  } catch (err) {
+    logWithStatus(KO, err.message, 1);
+    return;
   }
 
   // Run Web3Function
@@ -165,80 +305,5 @@ export default async function test(callConfig?: Partial<CallConfig>) {
     multiChainProviderConfig,
   });
 
-  // Show storage update
-  if (res.success && res.storage?.state === "updated") {
-    console.log(`\nSimulated Web3Function Storage update:`);
-    Object.entries(res.storage.storage).forEach(([key, value]) =>
-      console.log(` ${OK} ${key}: ${colors.green(`'${value}'`)}`)
-    );
-  }
-
-  // Show Web3Function result
-  console.log(`\nWeb3Function Result:`);
-  if (res.success) {
-    console.log(` ${OK} Return value:`, res.result);
-  } else {
-    console.log(` ${KO} Error: ${res.error.message}`);
-  }
-
-  // Show runtime stats
-  console.log(`\nWeb3Function Runtime stats:`);
-  if (res.throttled.duration) {
-    console.log(` ${KO} Duration: ${res.duration.toFixed(2)}s`);
-  } else if (res.duration > STD_TIMEOUT) {
-    console.log(
-      ` ${WARN} Duration: ${res.duration.toFixed(
-        2
-      )}s (Runtime is above Standard plan limit: ${STD_TIMEOUT}s!)`
-    );
-  } else {
-    console.log(` ${OK} Duration: ${res.duration.toFixed(2)}s`);
-  }
-  const memoryStatus = res.throttled.memory ? KO : OK;
-  console.log(` ${memoryStatus} Memory: ${res.memory.toFixed(2)}mb`);
-
-  if (res.success && res.throttled.storage) {
-    console.log(
-      ` ${KO} Storage: ${res.storage.size.toFixed(
-        2
-      )}kb - Storage usage exceeds limit!`
-    );
-  } else if (res.success && res.storage?.size > STD_STORAGE_LIMIT) {
-    console.log(
-      ` ${KO} Storage: ${res.storage.size.toFixed(
-        2
-      )}kb (Storage usage is above Standard plan limit: ${STD_STORAGE_LIMIT}kb!)`
-    );
-  } else if (res.success && res.storage?.size > 0) {
-    console.log(` ${OK} Storage: ${res.storage.size.toFixed(2)}kb`);
-  }
-
-  let networkMessage = ` ${res.throttled.networkRequest ? KO : OK} Network: ${
-    res.network.nbRequests
-  } req [${res.throttled.download ? KO : ""} DL: ${res.network.download.toFixed(
-    2
-  )}kb / UL: ${res.throttled.upload ? KO : ""} ${res.network.upload.toFixed(
-    2
-  )}kb]`;
-
-  if (res.throttled.networkRequest) {
-    networkMessage += ` ${`(${res.network.nbThrottled} req throttled - Please reduce your network usage!)`}`;
-  }
-  console.log(networkMessage);
-
-  if (res.throttled.rpcRequest) {
-    console.log(
-      ` ${KO} Rpc calls: ${
-        res.rpcCalls.total
-      } ${`(${res.rpcCalls.throttled} throttled - Please reduce your rpc usage!)`}`
-    );
-  } else if (res.rpcCalls.total > STD_RPC_LIMIT) {
-    console.log(
-      ` ${WARN} Rpc calls: ${
-        res.rpcCalls.total
-      } ${`(RPC usage is above Standard plan limit: ${STD_RPC_LIMIT}!)`}`
-    );
-  } else {
-    console.log(` ${OK} Rpc calls: ${res.rpcCalls.total}`);
-  }
+  logResult(res);
 }
