4ef70ea620aff6abd690767799efb05e6e0b6f76
Muhammed Kadir Yücel <kadir@gelato.digital>
2023-09-28 09:26:01 +0300

    test: add event log parsing

--- src/web3-functions/event/index.ts
+++ src/web3-functions/event/index.ts
@@ -3,11 +3,32 @@ import {
   Web3FunctionEventContext,
 } from "@gelatonetwork/web3-functions-sdk";
 
+import { Interface } from "@ethersproject/abi";
+
+const abi = [
+  `event ExecSuccess(
+        uint256 indexed txFee,
+        address indexed feeToken,
+        address indexed execAddress,
+        bytes execData,
+        bytes32 taskId,
+        bool callSuccess
+    )`,
+];
+
 Web3Function.onRun(async (context: Web3FunctionEventContext) => {
   const { log } = context;
 
   if (log.blockNumber !== 9727562) {
     throw new Error("Log file fetched invalid");
+  }
+
+  const contractInterface = new Interface(abi);
+
+  const description = contractInterface.parseLog(log);
+
+  if (description.name !== "ExecSuccess") {
+    throw new Error("Log is unexpected");
   }
 
   return {
